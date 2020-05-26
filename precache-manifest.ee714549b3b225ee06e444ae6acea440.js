31c7921cab29bc0be86bbcbf08d6042bf26c27e6
Tran Kim Thanh <kimthanh932000@gmail.com>
2020-05-26 10:58:12 +0700

    Updates

---
+++ precache-manifest.ee714549b3b225ee06e444ae6acea440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5ec1a2f69843ec65ad6e5953807491",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "087b512fecacb8d496fa",
+    "url": "/monster-rolodex/static/css/main.9ae8979d.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087b512fecacb8d496fa",
+    "url": "/monster-rolodex/static/js/main.734a9d9a.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
